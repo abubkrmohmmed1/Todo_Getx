a251ed3f237fb629dba86400345d0c5e99ba9976
unknown <abubkrmohmmed1@gmail.com>
2023-01-10 04:56:04 -0800

    Create a dart files contains constants of colors & icons

---
+++ lib/app/core/values/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const purple = Color(0xFF3F3C90);
+const orange = Color(0xFFF09D05);
+const pink = Color(0xFFF1A39A);
+const deepPink = Color(0xFFFA64C6);
+const green = Color(0xFF41CF9F);
+const yellow = Color(0xFFE3E312);
+const lightBlue = Color(0xFF42A5F5);
+const blue = Color(0xFF0D3FC2);
