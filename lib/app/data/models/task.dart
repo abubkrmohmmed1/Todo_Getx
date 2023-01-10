784c8c211925ca48b29a54249d1d5fd3cc29535c
Abu-Bakr <abubkrmohmmed1@gmail.com>
2023-01-10 07:39:20 -0800

    Setup a Serialization for Task class and emplement Equatable package

--- lib/app/data/models/task.dart
+++ lib/app/data/models/task.dart
@@ -1,4 +1,6 @@
-class Task{
+import 'package:equatable/equatable.dart';
+
+class Task extends Equatable{
   final String title;
   final int icon;
   final String color;
@@ -11,4 +13,19 @@ class Task{
     this.todos,
 });
 
+  factory Task.fromJson(Map<String, dynamic> json) => Task(
+    title: json['title'],
+    icon: json['icon'],
+    color: json['color'],
+    todos: json['todos'],
+  );
+  Map<String, dynamic> toJson() => {
+    'title': title,
+    'icon': icon,
+    'color': color,
+    'todos': todos,
+  };
+
+  @override
+  List<Object?> get props => [title, icon, color];
 }
